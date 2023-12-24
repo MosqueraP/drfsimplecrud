5b9742f1377c2ab9ddd4cfa7b51be8f55522f2d0
Plinio <mcplinio.15@gmail.com>
2023-12-24 16:53:16 -0500

    agregar proyecto

---
+++ projects/api.py
@@ -0,0 +1,8 @@
+from projects.models import Project
+from rest_framework import viewsets, permissions
+from  projects.serializers import ProjectSerializer
+
+class ProjecViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permissions_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
